5f9a75b077936124d722f073a7cc837ecfbd1456
Alex Angas <alexangas@gmail.com>
2020-05-04 12:49:25 +0100

    Jest coverage and test fixes

--- jest.config.js
+++ jest.config.js
@@ -6,12 +6,13 @@ module.exports = {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
-  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`, `\\cypress`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  collectCoverageFrom: [`src/**/*.js`],
   globals: {
     __PATH_PREFIX__: ``,
   },
   setupFiles: [`<rootDir>/loadershim.js`],
   reporters: ["default", "jest-junit"],
-  coverageReporters: ["cobertura", "html"],
+  coverageReporters: ["cobertura", "html", "text"],
 }
