255af58b46a5fb5f19994de0cf0d860353257231
Alex Angas <alexangas@gmail.com>
2020-05-04 15:32:27 +0100

    Added cypress (#12)
    * Added cypress
    
    * Added e2e to pipeline
    
    * e2e test fixes
    
    * Jest coverage and test fixes
    
    * npm Jest
    
    * CI dependencies
    
    * npm test
    
    * npm test:ci 1
    
    * Collect coverage from
    
    * Display name fix

---
+++ cypress/e2e/home.spec.js
@@ -0,0 +1,44 @@
+/// <reference types="Cypress" />
+
+describe("Root Home", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
+  describe("Accessibility tests", () => {
+    beforeEach(() => {
+      cy.get("main").injectAxe()
+    })
+    it("Has no detectable accessibility violations on load", () => {
+      cy.checkA11y()
+    })
+  })
+
+  describe("Header", () => {
+    it("Links to the home page", () => {
+      cy.get("header a")
+        .should("contain.text", "Alex Angas")
+        .should("have.attr", "href", "/")
+    })
+  })
+
+  describe("Contents", () => {
+    it("Contains a blog link", () => {
+      cy.findByText("Blog", { exact: false }).focus()
+      cy.focused()
+        .should("have.attr", "href", "/blog/")
+    })
+  })
+
+  describe("Footer", () => {
+    it("Contains the copyright message", () => {
+      cy.get("footer")
+        .should("contain.text", "©")
+        .should("contain.text", "Alex Angas")
+    })
+    it("Focuses on the footer link and asserts its attributes", () => {
+      cy.get('footer a')
+        .should("have.attr", "href", "https://www.gatsbyjs.org")
+    })
+  })
+})
