c69f6cc580bf7d8064b43ea0fdaf74443d670509
Alex Angas <alexangas@gmail.com>
2020-05-04 13:18:46 +0100

    npm test:ci 1

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,9 @@ module.exports = {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
-  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`, `\\cypress`],
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`, `cypress`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
-  collectCoverageFrom: [`src/**/*.js`],
+  //collectCoverageFrom: [`src/**/*.js`],
   globals: {
     __PATH_PREFIX__: ``,
   },
