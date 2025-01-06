ec45f682be34106f72d44c7a08b78a2935934af5
Alex Angas <alexangas@gmail.com>
2025-01-06 20:18:27 +0000

    feat: Astro 5 (#301)

---
+++ src/pages/manifest.json.ts
@@ -0,0 +1,33 @@
+import type { APIRoute } from 'astro'
+import { getImage } from 'astro:assets'
+import favicon from '../images/favicon.png'
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+
+const faviconPngSizes = [192, 512]
+
+export const GET: APIRoute = async () => {
+  const icons = await Promise.all(
+    faviconPngSizes.map(async (size) => {
+      const image = await getImage({
+        src: favicon,
+        width: size,
+        height: size,
+        format: 'png'
+      })
+      return {
+        src: image.src,
+        type: `image/${image.options.format}`,
+        sizes: `${image.options.width}x${image.options.height}`
+      }
+    })
+  )
+
+  const manifest = {
+    name: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    start_url: '/',
+    icons
+  }
+
+  return new Response(JSON.stringify(manifest))
+}
